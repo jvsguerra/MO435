52976d8888d9a66c57740ec8d17e0d2b6baa47ba
Joao Victor da Silva Guerra <ra117410@students.ic.unicamp.br>
2020-03-24 16:26:35 -0300

    Add multi-class-logistic-regression exercises

---
+++ dev/multi-class-logit/multi-class-scratch.py
@@ -0,0 +1,26 @@
+from create_datasets import Create_Datasets
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+class MultiClassLogisticRegression:
+
+    def __init__(self):
+        return
+    
+
+
+
+
+
+
+
+if __name__ == "__main__"
+    # Create dataset
+    dataset = Create_Datasets(150, method='classification', seed=0)
+
+    # Split in train and test partitions
+    dataset.split_test_and_training(test_frac=0.5)
+    # print(dataset.x)
+
